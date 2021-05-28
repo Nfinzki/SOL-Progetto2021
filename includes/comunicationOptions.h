c224ef435da392fad08f54ad3dd5d20f236e14af
Francesco Di Luzio <francescodiluzio07@gmail.com>
2021-05-28 16:03:34 +0200

    APPEND_FILE

--- includes/comunicationOptions.h
+++ includes/comunicationOptions.h
@@ -2,12 +2,13 @@
 
 #define _COMUNICATIONOPT_H
 
-#define OPEN_FILE 01
-#define CREATE_FILE 02
-#define FIND_FILE 03
-#define END_CONNECTION 04
-#define CLOSE_FILE 05
-#define SEND_FILE 06
-#define NO_ACTION 07
+#define OPEN_FILE 1
+#define CREATE_FILE 2
+#define FIND_FILE 3
+#define END_CONNECTION 4
+#define CLOSE_FILE 5
+#define SEND_FILE 6
+#define NO_ACTION 7
+#define APPEND_FILE 8
 
 #endif
