5c5b35ceeb7aee2e21964ac9cbaab969bb9078ba
Francesco Di Luzio <francescodiluzio07@gmail.com>
2021-05-30 16:53:05 +0200

    Rimosso un flag inutilizzato

--- includes/comunicationOptions.h
+++ includes/comunicationOptions.h
@@ -8,9 +8,8 @@
 #define END_CONNECTION 4
 #define CLOSE_FILE 5
 #define SEND_FILE 6
-#define NO_ACTION 7 //Probabilmente va tolto
-#define APPEND_FILE 8
-#define READ_FILE 9
-#define READN_FILE 10
+#define APPEND_FILE 7
+#define READ_FILE 8
+#define READN_FILE 9
 
 #endif
