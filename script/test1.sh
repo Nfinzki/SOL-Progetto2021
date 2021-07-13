858b7bb1e8aef10d14db35a328a79102de68332c
Francesco Di Luzio <francescodiluzio07@gmail.com>
2021-07-13 21:29:26 +0200

    Cambiamenti minori

--- script/test1.sh
+++ script/test1.sh
@@ -4,10 +4,15 @@ valgrind --leak-check=full ./bin/Server ./tmp/test1.txt &
 PID=$!
 sleep 1
 
+echo -e "\n\tClient per mostrare le opzioni\n"
+./bin/Client -h
+
 echo -e "\n\tEsecuzione Client 1\n"
 ./bin/Client -f tmp/socket.sk -w TestFolder/Canzoni,n=0 -D tmp -W TestFolder/prova.txt,TestFolder/file.txt -D tmp -l TestFolder/Canzoni/LaVieEnRose.txt,TestFolder/prova.txt -u TestFolder/Canzoni/LaVieEnRose.txt -p -t 200
+
 echo -e "\n\tEsecuzione Client 2\n"
 ./bin/Client -f tmp/socket.sk -R n=3 -d tmp -l TestFolder/Canzoni/LaVieEnRose.txt -r TestFolder/Canzoni/LaVieEnRose.txt -d tmp -W TestFolder/img/basket/DRose.jpg -D tmp -W TestFolder/img/NewYorkCity.jpg -p -t 200
+
 echo -e "\n\tEsecuzione Client 3\n"
 ./bin/Client -f tmp/socket.sk -l TestFolder/Canzoni/LaVieEnRose.txt -c TestFolder/Canzoni/LaVieEnRose.txt -p -t 200
 
